3baca8db8c292a072bf2b4e8849f7c72f1a1751a
Triyono <yono.tri@gmail.com>
2023-12-04 10:18:05 +0700

    add model 2

---
+++ app/Models/Brand2Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand2Model extends Model
+{
+    use HasFactory;
+}
